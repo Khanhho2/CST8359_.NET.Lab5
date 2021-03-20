25b7af5aba1a6817c30b492a2f7e6f24598237a0
Khanh Ho <khanhho7268@gmail.com>
2021-03-20 00:13:22 -0400

    Lab 5

---
+++ Data/AnswerImageDataContext.cs
@@ -0,0 +1,25 @@
+﻿using Lab5.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5.Data
+{
+    public class AnswerImageDataContext : DbContext
+    {
+        public AnswerImageDataContext(DbContextOptions<AnswerImageDataContext> options)
+           : base(options)
+        {
+
+        }
+
+        public DbSet<AnswerImage> AnswerImage { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AnswerImage>().ToTable("AnswerImage");
+        }
+    }
+}
